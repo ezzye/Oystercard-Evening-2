240a724daebb2a52f9a2ebf881ea519ef6b38383
ezzy elliott <ezzy.elliott@gmail.com>
2015-10-21 11:06:49 +0100

    challenge 13 done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,46 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station, :exit_station, :list_of_journeys
 
   def initialize (balance = 0)
     @balance = balance
+    @in_use = false
+    @entry_station = nil
+    @exit_station = nil
+    @list_of_journeys = []
   end
 
   def top_up amount
-    @balance += amount
+    raise "Over maximum balance of #{MAXIMUM_BALANCE}" if (@balance + amount) > MAXIMUM_BALANCE
+    self.balance += amount
   end
+
+  def in_journey?
+    entry_station
+  end
+
+  def touch_in station
+    raise "balance less than minimum fare of £#{MINIMUM_BALANCE}" if balance < MINIMUM_BALANCE
+    self.entry_station = station
+  end
+
+  def touch_out station
+    self.exit_station = station
+    self.list_of_journeys << {:entry_station => entry_station, :exit_station => exit_station}
+    self.entry_station = nil
+    deduct MINIMUM_BALANCE
+  end
+
+  private
+
+  def deduct amount
+    self.balance -= amount
+  end
+
+  attr_accessor :in_use
+  attr_writer :balance, :entry_station, :exit_station, :list_of_journeys
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station1) {double(:station1)}
+  let(:station2) {double(:station2)}
+  let (:journey){ {:entry_station => station1, :exit_station => station2} }
+
   it 'balance is zero when initialized' do
     expect(subject.balance).to eq 0
   end
@@ -10,7 +14,80 @@ describe Oystercard do
       expect {subject.top_up 1}.to change{subject.balance }.by 1
     end
     it 'error if over maximum balance' do
-      expect {subject.top_up (Oystercard::MAXIMUM_BALANCE +1)}.to raise_error "Over maximum balance of #{Oystercard::MAXIMUM_BALANCE}"
+      expect {subject.top_up((Oystercard::MAXIMUM_BALANCE) + 1)}.to raise_error "Over maximum balance of #{Oystercard::MAXIMUM_BALANCE}"
     end
   end
+  describe '#deduct' do
+    it 'balance decreases by deduct amount' do
+      subject.top_up Oystercard::MINIMUM_BALANCE
+      expect {subject.touch_out station2}.to change {subject.balance}.by -Oystercard::MINIMUM_BALANCE
+    end
+  end
+
+  describe 'touch in/out support' do
+    context 'when touch-in so in journey' do
+      it 'card touch-in' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        expect(subject).to be_in_journey
+      end
+    end
+    context 'when touch-out so not in journey' do
+      it 'card touch-out after being touched-in' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        subject.touch_out station2
+        expect(subject).not_to be_in_journey
+      end
+    end
+    context 'when not used to touch-in so not in journey' do
+      it 'not in journey if not being used' do
+        expect(subject).not_to be_in_journey
+      end
+    end
+    context 'when card does not cover minimum fare' do
+      it 'error raised' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_out station2
+        expect {subject.touch_in station1}.to raise_error "balance less than minimum fare of £#{Oystercard::MINIMUM_BALANCE}"
+      end
+    end
+    context 'when card touch-out deduct minimum balance' do
+      it 'touch out and deduct' do
+        expect {subject.touch_out station2}.to change {subject.balance}.by -Oystercard::MINIMUM_BALANCE
+      end
+    end
+    context 'when touch_in' do
+      it 'record station' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        expect(subject.entry_station).to eq station1
+      end
+    end
+    context 'when touch out' do
+      it 'records station' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        subject.touch_out station2
+        expect(subject.exit_station).to eq station2
+      end
+      it 'has empty list_of_journeys' do
+        expect(subject.list_of_journeys).to be_empty
+      end
+      it 'records hash of journey' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        subject.touch_out station2
+        expect(subject.list_of_journeys.count).to eq 1
+      end
+      it 'record list of journeys' do
+        subject.top_up Oystercard::MINIMUM_BALANCE
+        subject.touch_in station1
+        subject.touch_out station2
+        # expect(subject.list_of_journeys).to eq [{:entry_station => station1,:exit_station => station2}]
+        expect(subject.list_of_journeys).to include journey
+      end
+    end
+  end
+
 end
